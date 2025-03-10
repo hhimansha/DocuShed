bbd990e6f6b8f5f67a3c61da72c02010a3c14466
Haritha Himansha <143889589+hhimansha@users.noreply.github.com>
2025-03-10 23:29:18 +0530

    Project setup initial commit

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+function Home() {
+    return (
+      <div className="p-5 text-center text-2xl grid place-items-center mx-auto justify-center w-full h-full">
+        Welcone to AutoSched
+        <Button>Submit</Button>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
